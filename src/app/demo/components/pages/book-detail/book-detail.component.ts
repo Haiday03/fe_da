f5d15ff4011297d4b65710aeb18e9322a84596fb
van hai <vanhai200345@gmail.com>
2025-03-05 22:38:01 +0700

    Fix loi book-detail

--- src/app/demo/components/pages/book-detail/book-detail.component.ts
+++ src/app/demo/components/pages/book-detail/book-detail.component.ts
@@ -149,3 +149,156 @@ export class BookDetailComponent implements OnInit {
     }
 
 }
+
+//
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { AuthorDto } from 'src/app/demo/api/author/Author';
+import { BookRec } from 'src/app/demo/api/book/BookRec';
+import { Borrow } from 'src/app/demo/api/borrow/Borrow';
+import { CommentBook } from 'src/app/demo/api/comment/CommentBook';
+import { BorrowService } from 'src/app/demo/service/borrow.service';
+import { CartService } from 'src/app/demo/service/cart.service';
+import { WishService } from 'src/app/demo/service/wish.service';
+import { Book } from '../../../api/book/Book';
+import { BookService } from '../../../service/book.service';
+
+@Component({
+    templateUrl: './book-detail.component.html',
+    styleUrls: ['./book-detail.component.css'],
+    providers: [MessageService]
+})
+export class BookDetailComponent implements OnInit {
+    selectedQuantity = 1;
+
+    dialogConfirmBorrow = false;
+
+    book: Book;
+    author: AuthorDto;
+    publisher: any;
+    submitted = false;
+
+    comments: CommentBook[];
+
+    recommended: BookRec[];
+
+    listReviews : Borrow[] = [];
+    
+    responsiveOptions;
+
+    bookId:string="";
+    title: string="";
+    content: string="";
+
+
+    currentUser:{};
+    userId: string="";
+    username: string="";
+
+    constructor(private cartService: CartService,
+        private route: ActivatedRoute,
+        private messageService: MessageService,
+        private bookService: BookService,
+        private router: Router,
+        private wishService: WishService,
+        private borrowService: BorrowService,
+    ){}
+
+    ngOnInit(): void {
+
+        this.responsiveOptions = [
+            {
+                breakpoint: '1024px',
+                numVisible: 3,
+                numScroll: 3
+            },
+            {
+                breakpoint: '768px',
+                numVisible: 2,
+                numScroll: 2
+            },
+            {
+                breakpoint: '560px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ];
+
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser')!);
+        this.username=this.currentUser['username'];
+    
+        this.bookId = this.route.snapshot.paramMap.get('id')!;
+
+        this.bookService.getById(this.bookId).subscribe(
+            res=>{
+                this.book=res;
+            },
+            error =>{
+                this.messageService.add({ severity: 'error', summary: 'Thất bại', detail: 'Có lỗi xảy ra trong quá trình xử lý!', life: 3000 });
+            }
+        );
+
+        this.borrowService.getReviewsByBookId(this.bookId).subscribe(
+            res=>{
+                this.listReviews=res;
+            },
+            error =>{
+                this.messageService.add({ severity: 'error', summary: 'Thất bại', detail: 'Có lỗi xảy ra trong quá trình xử lý!', life: 3000 });
+            }
+        );
+       
+    }
+
+    addBookToCart(bookId: number){
+        this.cartService.post(bookId).subscribe(
+            res=>{
+                this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Book is added to cart', life: 3000 });
+            }, 
+            error =>{
+                this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Book cannot be added to cart', life: 3000 });
+            }     
+        )
+    };
+
+    changeSource(event) {      
+        event.target.src = "assets/demo/images/product/noimage.jpg";
+    }
+
+    bookDetail(bookId:number){
+        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+        this.router.onSameUrlNavigation = 'reload';
+        this.router.navigate(['/pages/book-detail',bookId]);
+    }
+
+    addBookToWish(bookId: number){
+        this.wishService.post(bookId).subscribe(
+            res=>{
+                this.messageService.add({ severity: 'success', summary: 'Thành công', detail: 'Sách đã được thêm vào yêu thích!', life: 3000 });
+            },
+            error =>{
+                this.messageService.add({ severity: 'error', summary: 'Thất bại', detail: error, life: 3000 });
+            }     
+        )
+    };
+
+    openConfirmBorrowDialog(){
+        this.dialogConfirmBorrow = true;
+    }
+
+    confirmBorrow(bookId: number){
+        let borrow = new Borrow(bookId, this.selectedQuantity, 1, this.username);
+
+        this.borrowService.post(borrow).subscribe(
+            res => {
+                this.messageService.add({ severity: 'success', summary: 'Thành công', detail: 'Đặt mượn sách thành công!', life: 3000 });
+                this.dialogConfirmBorrow = false;
+            }, 
+            error => {
+                this.messageService.add({ severity: 'error', summary: 'Thất bại', detail: error, life: 3000 });
+                this.dialogConfirmBorrow = false;
+            }
+        )
+    }
+
+}
--- src/app/demo/components/pages/book-detail/book-detail.module.ts
+++ src/app/demo/components/pages/book-detail/book-detail.module.ts
@@ -51,3 +51,59 @@ import {TableModule} from 'primeng/table';
 })
 export class BookDetailModule {
  }
+
+
+ // 
+ import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookDetailComponent } from './book-detail.component';
+import { BookDetailRoutingModule } from './book-detail-routing.module';
+import {ToastModule} from 'primeng/toast';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {RatingModule} from 'primeng/rating';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {InputTextModule} from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {DividerModule} from 'primeng/divider';
+import {ImageModule} from 'primeng/image';
+import {CarouselModule} from 'primeng/carousel';
+import { TooltipModule } from 'primeng/tooltip';
+import { TabViewModule } from 'primeng/tabview';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogModule } from 'primeng/dialog';
+import { CurrencyReverseModule } from '../../../common/pipes/currency-reverse.module'
+import { PanelModule } from 'primeng/panel';
+import { AvatarModule } from 'primeng/avatar';
+import { FieldsetModule } from 'primeng/fieldset';
+import {TableModule} from 'primeng/table';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        BookDetailRoutingModule,
+        ReactiveFormsModule,
+        ToastModule,
+        CardModule,
+        ButtonModule,
+        RatingModule,
+        InputTextareaModule,
+        InputTextModule,
+        DividerModule,
+        ImageModule,
+        CarouselModule,
+        TooltipModule,
+        TabViewModule,
+        InputNumberModule,
+        DialogModule,
+        CurrencyReverseModule,
+        PanelModule,
+        AvatarModule,
+        FieldsetModule,
+        TableModule
+    ],
+    declarations: [BookDetailComponent]
+})
+export class BookDetailModule {
+ }
